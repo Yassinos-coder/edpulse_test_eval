480cee330c303b6eddf29ce10cc299a2427cf5ed
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-03-30 00:07:45 +0000

    Quick backend port fix

--- backend/core.js
+++ backend/core.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8009;
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 
